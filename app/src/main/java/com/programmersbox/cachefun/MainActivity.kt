b4fdefa8e32b8e549825d58d1e9461afef56dbd5
jakepurple13 <jakerein@gmail.com>
2022-04-01 08:34:12 -0400

    - added custom dsl for the genericcache

--- app/src/main/java/com/programmersbox/cachefun/MainActivity.kt
+++ app/src/main/java/com/programmersbox/cachefun/MainActivity.kt
@@ -9,16 +9,14 @@ import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import com.programmersbox.cachefun.ui.theme.CacheFunTheme
 import java.util.concurrent.TimeUnit
+import kotlin.properties.Delegates
 
 class MainActivity : ComponentActivity() {
 
@@ -26,27 +24,32 @@ class MainActivity : ComponentActivity() {
 
     private val map = mutableStateMapOf<Int, String>()
 
-    private val cache = ExpirableLRUCache<Int, String>(
-        object : GenericCache<Int, String> {
-            override val size: Int
-                get() = map.size
+    /*private val cache = ExpirableLRUCache(
+        delegate = object : GenericCache<Int, String> {
+            override val size: Int get() = map.size
 
             override fun set(key: Int, value: String) {
                 map[key] = value
             }
 
             override fun get(key: Int): String? = map[key]
-
             override fun remove(key: Int): String? = map.remove(key)
-
-            override fun clear() {
-                map.clear()
-            }
-
+            override fun clear() = map.clear()
         },
-        5,
-        TimeUnit.MINUTES.toMillis(5)
-    )
+        minimalSize = 5,
+        flushInterval = TimeUnit.MINUTES.toMillis(5)
+    )*/
+
+    private val cache = ExpirableLRUCache<Int, String>(
+        minimalSize = 5,
+        flushInterval = TimeUnit.MINUTES.toMillis(5)
+    ) {
+        size = map.size
+        set { key, value -> map[key] = value }
+        get { map[it] }
+        remove { map.remove(it) }
+        clear { map.clear() }
+    }
 
     @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,11 +58,7 @@ class MainActivity : ComponentActivity() {
             CacheFunTheme {
                 // A surface container using the 'background' color from the theme
                 Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = { Text("Cache Fun") }
-                        )
-                    },
+                    topBar = { TopAppBar(title = { Text("Cache Fun") }) },
                     bottomBar = {
                         BottomAppBar {
                             OutlinedButton(
@@ -69,18 +68,14 @@ class MainActivity : ComponentActivity() {
                     }
                 ) { p ->
 
-                    LazyColumn(
-                        contentPadding = p
-                    ) {
+                    LazyColumn(contentPadding = p) {
 
                         item {
                             val time by currentTime()
                             Text("The current time is: ${SimpleDateFormat.getDateTimeInstance().format(time)}")
                         }
 
-                        items(map.toList()) {
-                            Text("${it.first} == ${it.second}")
-                        }
+                        items(map.toList()) { Text("${it.first} == ${it.second}") }
 
                     }
 
@@ -89,6 +84,9 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@DslMarker
+annotation class GenericCacheMarker
 
 /**
  * A Generic K,V [GenericCache] defines the basic operations to a cache.
@@ -118,17 +116,62 @@ interface GenericCache<K, V> {
      * Remove all the items in the cache.
      */
     fun clear()
+
+    class GenericCacheBuilder<K, V> {
+        @GenericCacheMarker
+        var size: Int by Delegates.notNull()
+        private var set: (key: K, value: V) -> Unit by Delegates.notNull()
+        private var get: (key: K) -> V? by Delegates.notNull()
+        private var remove: (key: K) -> V? by Delegates.notNull()
+        private var clear: () -> Unit by Delegates.notNull()
+
+        @GenericCacheMarker
+        fun set(block: (key: K, value: V) -> Unit) {
+            set = block
+        }
+
+        @GenericCacheMarker
+        fun get(block: (key: K) -> V?) {
+            get = block
+        }
+
+        @GenericCacheMarker
+        fun remove(block: (key: K) -> V?) {
+            remove = block
+        }
+
+        @GenericCacheMarker
+        fun clear(block: () -> Unit) {
+            clear = block
+        }
+
+        internal fun build() = object : GenericCache<K, V> {
+            override val size: Int get() = this@GenericCacheBuilder.size
+            override fun set(key: K, value: V) = this@GenericCacheBuilder.set(key, value)
+            override fun get(key: K): V? = this@GenericCacheBuilder.get(key)
+            override fun remove(key: K): V? = this@GenericCacheBuilder.remove(key)
+            override fun clear() = this@GenericCacheBuilder.clear()
+        }
+    }
 }
+
 
 /**
  * [ExpirableLRUCache] flushes items that are **Least Recently Used** and keeps [minimalSize] items at most
  * along with flushing the items whose life time is longer than [flushInterval].
  */
 class ExpirableLRUCache<K, V>(
-    private val delegate: GenericCache<K, V>,
     private val minimalSize: Int = DEFAULT_SIZE,
-    private val flushInterval: Long = TimeUnit.MINUTES.toMillis(1)
+    private val flushInterval: Long = TimeUnit.MINUTES.toMillis(1),
+    private val delegate: GenericCache<K, V>,
 ) : GenericCache<K, V> by delegate {
+
+    constructor(minimalSize: Int, flushInterval: Long, delegate: GenericCache.GenericCacheBuilder<K, V>.() -> Unit) : this(
+        minimalSize,
+        flushInterval,
+        GenericCache.GenericCacheBuilder<K, V>().apply(delegate).build()
+    )
+
     private val keyMap = object : LinkedHashMap<K, Boolean>(minimalSize, .75f, true) {
         override fun removeEldestEntry(eldest: MutableMap.MutableEntry<K, Boolean>): Boolean {
             val tooManyCachedItems = size > minimalSize
